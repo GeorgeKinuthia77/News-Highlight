2f176d67ce76aca5f1d5b631cd719e2f1714b7f3
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-07-31 15:51:45 +0300

    changes in config.py

--- config.py
+++ config.py
@@ -2,12 +2,10 @@ import os
 
 class Config:
 
-    
+
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     NEWS_ARTICLE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    
-
 
 
     @staticmethod
